6cb87d3f38ad6ce50a28a126da27b4ceeeda2cc1
mcps5601 <mcps5601@gmail.com>
2019-04-08 22:40:43 +0800

    script for PyTorch installation

---
+++ pytorch_install.sh
@@ -0,0 +1,8 @@
+# Linux with GPU
+pip install https://download.pytorch.org/whl/cu80/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+
+# Linux without GPU
+# pip install https://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
+
+# essential
+pip install torchvision==0.2.1
